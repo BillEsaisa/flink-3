9b10911b11dd0dd0ea50173341f51e81a27cc9f6
BillEsaisa <BillEsaisa@163.com>
2022-06-14 16:38:30 +0800

    first commit test

---
+++ src/main/scala/com/atgui/Flink_helloworld.scala
@@ -0,0 +1,26 @@
+package com.atgui
+
+import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.api.scala._
+
+object Flink_helloworld {
+  def main(args: Array[String]): Unit = {
+    //创建批处理的执行环境
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+    //引入文件路径，读取文件
+    val inpath ="E:\\Ideaproject\\flink-3\\src\\main\\resources\\test.txt"
+    val inputdataset: DataSet[String] = env.readTextFile(inpath)
+    //基于批处理的dataset进行批处理
+
+    val result: AggregateDataSet[(String, Int)] = inputdataset.flatMap(_.split(" "))
+      .map((_, 1))
+      .groupBy(0)
+      .sum(1)
+
+    result.print()
+
+
+
+  }
+
+}
